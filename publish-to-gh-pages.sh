038548333f1ff685569a570742eb40ec7555f1be
lovelyant <yan.guangwei@qq.com>
2019-07-03 22:41:05 +0800

    travis conf

---
+++ publish-to-gh-pages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ev
+git clone https://${GH_REF} .deploy_git
+cd .deploy_git
+git checkout master
+cd ../
+mv .deploy_git/.git/ ./public/
+cd ./public
+git config user.name  "lovelyant"
+git config user.email "yan.guangwei@qq.com" 
+# add commit timestamp
+git add .
+git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M"`"
+git push --force --quiet "https://${Travis_gh_token}@${GH_REF}" master:master
